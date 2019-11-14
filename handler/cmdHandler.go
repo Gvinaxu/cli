0342bd9419a2ba118ba8b0cb5fc9de0634f2de71
gavin <gvinaxu@bcanalyst.com>
2019-11-14 18:09:33 +0800

    add  file cp mv rm mkdir

--- handler/cmdHandler.go
+++ handler/cmdHandler.go
@@ -17,13 +17,79 @@ func NewHandler(file *FileReq) *Handler {
 
 func (h *Handler) InvokeCmd(command string, args []string) {
 	cmd := strings.ToLower(command)
+	var err error
 	switch cmd {
 	case "tree", "t":
 		path := ""
 		if len(args) != 0 {
 			path = args[0]
 		}
-		h.file.Tree(path)
+		_, err = h.file.Tree(path)
+	case "stat":
+		path := ""
+		if len(args) != 0 {
+			path = args[0]
+		} else {
+			fmt.Println("file path is nil")
+			return
+		}
+		_, err = h.file.Stat(path)
+	case "move", "mv":
+		old, new := "", ""
+		if len(args) == 2 {
+			old = args[0]
+			new = args[1]
+		} else {
+			fmt.Println("parameter is missing")
+			return
+		}
+		err = h.file.Mv(old, new)
+	case "copy", "cp":
+		old, new := "", ""
+		if len(args) == 2 {
+			old = args[0]
+			new = args[1]
+		} else {
+			fmt.Println("parameter is missing")
+			return
+		}
+		err = h.file.Cp(old, new)
+	case "find":
+		path := ""
+		search := ""
+		if len(args) >= 1 {
+			search = args[0]
+		} else {
+			fmt.Println("search  is nil")
+			return
+		}
+		if len(args) >= 2 {
+			path = args[1]
+		}
+		_, err = h.file.Search(search, path)
+	case "remove", "rm":
+		path := ""
+		if len(args) != 0 {
+			path = args[0]
+		} else {
+			fmt.Println("file path is nil")
+			return
+		}
+		err = h.file.Rm(path)
+	case "mkdir":
+		path := ""
+		if len(args) != 0 {
+			path = args[0]
+		}
+		if path == "" {
+			fmt.Println("file path is nil")
+			return
+		}
+		err = h.file.Mkdir(path)
+	case "upload", "up":
+
+	case "download", "down":
+
 	case "help", "h":
 		h.printHelp()
 	case "":
@@ -31,6 +97,9 @@ func (h *Handler) InvokeCmd(command string, args []string) {
 	default:
 		fmt.Printf("command %s not exists \n", command)
 		h.printHelp()
+	}
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
--- handler/file.go
+++ handler/file.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/Gvinaxu/cli/util"
 	"github.com/valyala/fasthttp"
@@ -13,6 +14,12 @@ import (
 
 const (
 	treeUrl = "http://127.0.0.1:18080/api/1/file/list"
+	mvUrl   = "http://127.0.0.1:18080/api/1/file/mv"
+	cpUrl   = "http://127.0.0.1:18080/api/1/file/cp"
+	rmUrl   = "http://127.0.0.1:18080/api/1/file/delete"
+
+	mkdirUrl  = "http://127.0.0.1:18080/api/1/file/mkdir"
+	searchUrl = "http://127.0.0.1:18080/api/1/search/search"
 )
 
 var (
@@ -29,7 +36,7 @@ func (f *FileReq) Tree(path string) ([]*File, error) {
 	head := map[string]interface{}{
 		"access_token": token.AccessToken,
 	}
-	fmt.Println(path)
+
 	code, body, err := util.DoTimeout(args, "POST", treeUrl, head)
 	if err != nil {
 		return nil, err
@@ -49,7 +56,7 @@ func (f *FileReq) Tree(path string) ([]*File, error) {
 	if path == "" {
 		path = "/"
 	}
-
+	fmt.Println()
 	fmt.Println(path)
 	f.printDirectory(r.Data, 0)
 	fmt.Println()
@@ -75,6 +82,204 @@ func (f *FileReq) printListing(entry string, depth int, dir bool) {
 	indent := strings.Repeat("|   ", depth)
 	fmt.Printf("%s|-- %s\n", indent, entry)
 	// output color
+}
+
+func (f *FileReq) Stat(path string) (*File, error) {
+	args := &fasthttp.Args{}
+	args.Add("path", path)
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", treeUrl, head)
+	if err != nil {
+		return nil, err
+	}
+	if code == http.StatusNotFound {
+		errMsg := fmt.Sprintf("path %s file not found ", path)
+		fmt.Println(errMsg)
+		return nil, errors.New(errMsg)
+	}
+	if code != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int     `json:"code"`
+		Data []*File `json:"data"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return nil, err
+	}
+	file := r.Data[0]
+
+	tm := time.Unix(file.Time, 0)
+	fmt.Println()
+	fmt.Printf("File: %s \n", file.Name)
+
+	fmt.Printf("Size: %d	Blocks:%d  Time:%s	", file.Size, len(file.Blocks), tm.Format("2006-01-02 15:04:05"))
+	if file.Dir {
+		fmt.Printf("Regular:dir \n")
+	} else {
+		fmt.Printf("Regular:file \n")
+	}
+	fmt.Println()
+	return file, nil
+}
+
+func (f *FileReq) Mkdir(path string) error {
+	args := &fasthttp.Args{}
+	args.Add("path", path)
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", mkdirUrl, head)
+	if err != nil {
+		return err
+	}
+	if code != http.StatusOK {
+		return errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int `json:"code"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return err
+	}
+	if r.Code != 0 {
+		return errors.New("mkdir fail")
+	}
+	return nil
+}
+
+func (f *FileReq) Cp(oldPath, newPath string) error {
+	args := &fasthttp.Args{}
+	args.Add("path", oldPath)
+	args.Add("tagPath", newPath)
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", cpUrl, head)
+	if err != nil {
+		return err
+	}
+	if code != http.StatusOK {
+		return errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int `json:"code"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return err
+	}
+	if r.Code != 0 {
+		return errors.New("copy fail")
+	}
+	return nil
+}
+
+func (f *FileReq) Mv(oldPath, newPath string) error {
+	args := &fasthttp.Args{}
+	args.Add("path", oldPath)
+	args.Add("tagPath", newPath)
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", mvUrl, head)
+	if err != nil {
+		return err
+	}
+	if code != http.StatusOK {
+		return errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int `json:"code"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return err
+	}
+	if r.Code != 0 {
+		return errors.New("move fail")
+	}
+	return nil
+}
+
+func (f *FileReq) Rm(path string) error {
+	args := &fasthttp.Args{}
+	args.Add("path", path)
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", rmUrl, head)
+	if err != nil {
+		return err
+	}
+	if code != http.StatusOK {
+		return errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int `json:"code"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return err
+	}
+	if r.Code != 0 {
+		return errors.New("remove fail")
+	}
+	return nil
+}
+
+func (f *FileReq) Search(search, path string) ([]*File, error) {
+	args := &fasthttp.Args{}
+	args.Add("search", search)
+	if path != "" {
+		args.Add("path", path)
+	}
+	head := map[string]interface{}{
+		"access_token": token.AccessToken,
+	}
+
+	code, body, err := util.DoTimeout(args, "POST", searchUrl, head)
+	if err != nil {
+		return nil, err
+	}
+	if code != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("response code is %d", code))
+	}
+	type R struct {
+		Code int     `json:"code"`
+		Data []*File `json:"data"`
+	}
+	r := &R{}
+	err = json.Unmarshal(body, r)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println()
+	if len(r.Data) == 0 {
+		fmt.Println("result 0 row")
+	} else {
+		fmt.Println("name			path")
+	}
+
+	for _, v := range r.Data {
+		fmt.Printf("%s		%s \n", v.Name, v.Path)
+	}
+	return r.Data, nil
+
 }
 
 type File struct {
